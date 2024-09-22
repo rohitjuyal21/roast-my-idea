b18e280324e3bc75d72cc4ef0e0569838483c479
rohitjuyal21 <rohitjuyal2003@gmail.com>
2024-09-22 13:27:08 +0530

    Update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
+import { auth } from "./auth";
 
 export async function middleware(req: NextRequest) {
+  const session = await auth();
+  const user = session?.user;
+
+  if (!user) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
   // const token = await getToken({
   //   req,
   //   secret: process.env.AUTH_SECRET!,
   // });
 
-  // console.log("Token in middleware:", token);
-  // console.log("Auth Secret", process.env.AUTH_SECRET);
-  // console.log("google id", process.env.AUTH_GOOGLE_ID);
-  // console.log("google secret", process.env.AUTH_GOOGLE_SECRET);
+  console.log("user", user);
+  console.log("Auth Secret", process.env.AUTH_SECRET);
+  console.log("google id", process.env.AUTH_GOOGLE_ID);
+  console.log("google secret", process.env.AUTH_GOOGLE_SECRET);
 
   // if (!token) {
   //   return NextResponse.redirect(new URL("/login", req.nextUrl));
