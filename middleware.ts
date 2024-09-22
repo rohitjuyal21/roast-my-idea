9362b9218ec24154d4850be4e641513c316a85fb
rohitjuyal21 <rohitjuyal2003@gmail.com>
2024-09-22 18:16:42 +0530

    Update milldleware

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
+import { auth } from "./auth";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({
-    req,
-    secret: process.env.AUTH_SECRET!,
-  });
-
+  const session = await auth();
   const { pathname } = req.nextUrl;
 
   if (pathname.startsWith("/login")) {
     return NextResponse.next();
   }
 
-  console.log("user", token);
+  console.log("user", session);
   console.log("Auth Secret", process.env.AUTH_SECRET);
   console.log("google id", process.env.AUTH_GOOGLE_ID);
   console.log("google secret", process.env.AUTH_GOOGLE_SECRET);
 
-  if (!token) {
+  if (!session?.user) {
     return NextResponse.redirect(new URL("/login", req.nextUrl));
   }
 
@@ -26,5 +22,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/settings/:path*", "/saved/:path*", "/:path/comments/", "/"],
 };
