4ffec6a9a2cb0fb80432add7942dfe3865073975
rohitjuyal21 <rohitjuyal2003@gmail.com>
2024-09-22 14:14:00 +0530

    Update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
+export const config = {
+  matcher: ["/settings/:path*", "/saved/:path*", "/:path/comments/", "/"],
+};
+
 export async function middleware(req: NextRequest) {
   const token = await getToken({
     req,
@@ -12,12 +16,9 @@ export async function middleware(req: NextRequest) {
   console.log("google id", process.env.AUTH_GOOGLE_ID);
   console.log("google secret", process.env.AUTH_GOOGLE_SECRET);
 
-  // if (!token) {
-  //   return NextResponse.redirect(new URL("/login", req.nextUrl));
-  // }
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
 
   return NextResponse.next();
 }
-export const config = {
-  matcher: ["/settings/:path*", "/saved/:path*", "/:path/comments/", "/"],
-};
