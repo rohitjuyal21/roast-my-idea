5f8d0d6c77008452b5fbb34598fabaec0f5fcb68
rohitjuyal21 <rohitjuyal2003@gmail.com>
2024-09-23 08:41:03 +0530

    Update middleware

--- middleware.ts
+++ middleware.ts
@@ -2,21 +2,29 @@ import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({
-    req,
-    secret: process.env.AUTH_SECRET!,
-  });
+  try {
+    const token = await getToken({
+      req,
+      secret: process.env.AUTH_SECRET as string,
+    });
 
-  console.log("user", token);
-  console.log("Auth Secret", process.env.AUTH_SECRET);
-  console.log("google id", process.env.AUTH_GOOGLE_ID);
-  console.log("google secret", process.env.AUTH_GOOGLE_SECRET);
+    console.log("User token:", token);
+    console.log("Auth Secret:", process.env.AUTH_SECRET);
+    console.log("Google ID:", process.env.AUTH_GOOGLE_ID);
+    console.log("Google Secret:", process.env.AUTH_GOOGLE_SECRET);
 
-  // if (!token) {
-  //   return NextResponse.redirect(new URL("/login", req.nextUrl));
-  // }
+    if (!token) {
+      console.log("No token found, redirecting to login page");
+      return NextResponse.redirect(new URL("/login", req.nextUrl));
+    }
 
-  return NextResponse.next();
+    console.log("Token found, proceeding to next middleware");
+    return NextResponse.next();
+  } catch (error) {
+    console.error("Error in middleware:", error);
+    // Decide how to handle errors - you might wasnt to redirect to an error page
+    return NextResponse.redirect(new URL("/", req.nextUrl));
+  }
 }
 
 export const config = {
