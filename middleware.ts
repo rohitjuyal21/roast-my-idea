8b5e3a760289544ba1a5e5521c7973e88405846c
rohitjuyal21 <rohitjuyal2003@gmail.com>
2024-09-28 22:52:22 +0530

    Updated middleware

--- middleware.ts
+++ middleware.ts
@@ -11,4 +11,11 @@ export const config = {
     "/login",
     "/",
   ],
+  runtime: "nodejs",
+  unstable_allowDynamic: [
+    // allows a single file
+    "/lib/db.ts",
+    // use a glob to allow anything in the function-bind 3rd party module
+    "/node_modules/mongoose/dist/**",
+  ],
 };
